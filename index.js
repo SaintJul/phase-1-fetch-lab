4d784066e0930054957f6c3b220a335e34ebb880
SaintJul <favor.julius@gmail.com>
2021-05-29 06:07:38 -0400

    commit tree d8a051bdd7c8fc0e77959ec905e1b04df9e30aa6

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json))
+
+  //.then(json => console.log(json[1]['characters']['597']));
+  /*
+  .then(json => console.log(json[0]["numberOfPages"] + json[1]["numberOfPages"] + 
+  json[2]["numberOfPages"] + json[3]["numberOfPages"] + json[4]["numberOfPages"] + 
+  json[5]["numberOfPages"] + json[6]["numberOfPages"] + json[7]["numberOfPages"] + 
+  json[8]["numberOfPages"] + json[9]["numberOfPages"]))
+  */
 }
 
 function renderBooks(books) {
